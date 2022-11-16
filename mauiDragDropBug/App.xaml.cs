563c2c643902e7f2a5d89a08a48d137087bbf590
Rene Moc <renemoc@live.de>
2022-11-16 15:04:59 +0100

    Init

---
+++ mauiDragDropBug/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace mauiDragDropBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ mauiDragDropBug/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace mauiDragDropBug;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void OnDragStarting(object sender, DragStartingEventArgs e)
+    {
+        Image image = (sender as Element).Parent as Image;
+        if (image != null)
+            e.Data.Image = image.Source;        
+    }
+
+    async void OnDrop(object sender, DropEventArgs e)
+    {
+        var imgSource = await e.Data.GetImageAsync();
+        if (!ImageSource.IsNullOrEmpty(imgSource))
+        {
+            Image image = (sender as Element).Parent as Image;
+            if (image != null)
+                image.Source = imgSource;
+        }
+    }
+}
+
